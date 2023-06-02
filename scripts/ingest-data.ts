76ef7f55e2002908c053c6a14ee203ac84056ea9
humblefirm <38067691+humblefirm@users.noreply.github.com>
2023-06-02 22:57:57 +0900

    optimize

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,3 +1,5 @@
+import natural from 'natural';
+import * as stopword from 'stopword';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
@@ -6,36 +8,40 @@ import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 
-/* Name of directory to retrieve your files from 
-   Make sure to add your PDF files inside the 'docs' folder
-*/
 const filePath = 'docs';
+
+const tokenizer = new natural.WordTokenizer();
+const stemmer = natural.PorterStemmer;
 
 export const run = async () => {
   try {
-    /*load raw docs from the all files in the directory */
     const directoryLoader = new DirectoryLoader(filePath, {
       '.pdf': (path) => new PDFLoader(path),
     });
 
-    // const loader = new PDFLoader(filePath);
     const rawDocs = await directoryLoader.load();
 
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+    // Normalize, remove stopwords, and stem the text
+    const processedDocs = rawDocs.map((doc) => {
+      let text = String(doc).toLowerCase();
+      let tokens = tokenizer.tokenize(text);
+      tokens = stopword.removeStopwords(tokens as string[]);
+      tokens = tokens.map((token) => stemmer.stem(token));
+      return { ...doc, text: tokens.join(' ') };
     });
 
-    const docs = await textSplitter.splitDocuments(rawDocs);
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 2000,
+      chunkOverlap: 400,
+    });
+
+    const docs = await textSplitter.splitDocuments(processedDocs);
     console.log('split docs', docs);
 
     console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
     const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
 
-    //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
       namespace: PINECONE_NAME_SPACE,
